449e1e31f36d1bf307144ae043c08684fc37eaa4
Ben W <Bwesterhof14@gmail.com>
2022-04-26 18:17:48 -0400

    Final commit. Documentation and final code

--- README.md
+++ README.md
@@ -4,6 +4,104 @@ All the code needed to read a Seeed 101020634 combination C02, temperature, and 
 # Installation
 To install this code on a raspberry pi, wire the pi according to the fritzing diagram and wiring guide included in this repo
 
-Next, save a copy of serviceAccountKey.json to the pi's microSD card. This file includes login credentials for the database, without this file the Pi will be unable to connect to the website. For security reasons, it is not included with the repo, but should be saved locally to existing Pi units.
-
 Once the file is copied over to the Pi, power on the pi, and enter <pip install firebase-admin> onto the Pi's command line. This will install the database's SDK.
+
+# Setting up the Pi
+1. Start by downloading and running the Raspberry Pi imager here: [https://www.raspberrypi.com/software/](https://www.raspberrypi.com/software/)
+2. Install a copy of Raspberry Pi OS Lite (32-bit) under the Raspberyy Pi OS (other) category
+3. Install the OS onto the inserted SD card
+    -Note that due to a strange hardware bug on window's SD card readers, there is a small chance doing this will crash your computer, usually once the status bar is marked as complete, if this happens, reboot and eject the SD card, it finished copying the OS properly, its just that some SD card readers have a design flaw that causes this crash.
+
+3. Connect the sensor to the Pi's pins according to SensorBox_bb.PNG, using pinout-.jpg as a guide if needed.
+4. Connect the camera to the Pi by pulling the black tab on the slot opposite the micro-usb slot, and inserting the camera's cable so the lens faces upwards
+5. Connect the Pi to a monitor and keyboard using a HDMI to micro HDMI cord, and a USB to micro USB adapter, and power it on
+
+At this point, everything else needs to be done on the raspberry pi itself, so the next instructions will be for command line set-up.
+
+# Command Line
+Note: This section provides my best guess on how to set up wifi using only a Raspberry Pi Zero W. Initially, wifi was set up using a Raspberry Pi 3, if these instructions fail on the provided Pi zeros, it may be possible to borrow a Pi 3 from Professor Schuurman. 
+
+To log into the pi for the first time, use the default username/password of pi/raspberry.
+To change this, run the command <passwd> and follow the prompts
+
+# Wifi
+If this is necessary, repeat these instructions with the SD card plugged into a Pi 3, and start by connecting it to ethernet.
+1. Type <sudo nano /etc/network/interfaces> and add the following block of code below the pound signs:
+<iface eth0 inet manual
+allow-hotplug wlan0
+iface wlan0 inet dhcp
+    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf>
+
+2. Press ctrl+S to save the file, and ctrl+X to exit nano
+3. a. To generate an NTHASH of a password, run <echo -n <password> | iconv -t utf16le | openssl md4> on the main command line. Make a note of the output.
+3. b. Run <sudo nano /etc/wpa_supplicant/wpa_supplicant.conf> and at the bottom of the file add the following:
+<ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+ctrl_interface_group=0
+update_config=1
+country=US
+network={
+      ssid="eduroam"
+      scan_ssid=1
+      key_mgmt=WPA-EAP
+      eap=PEAP
+      identity="username@students.calvin.edu"
+      password=hash:<HashValue>           <--- NTHASH of password -- fill in with your generated one from above
+      phase2="MSCHAPV2"
+   }>
+
+4. Run <sudo reboot> to turn the pi off and back on again. 
+
+Note: As of right now, the password hash present in the Pi is a hash of my password, which is expiring soon, and can either be replaced with another student's email password, or by a CIT generated password. However, the former option is a lot easier.
+
+# Packages
+    Next, we have to install all relevant packages our program uses.
+0. Run <sudo apt-get update>, then <sudo apt-get upgrade> to ensure the OS is up to date, this will take a while.
+1. Run <sudo pip install requests>
+2. Run <sudo pip install scd30_i2c>
+3. Run <sudo pip install oauth2client>
+4. Run <sudo pip install subprocess>
+5. Run <sudo  pip install picamera>
+
+# App set-up
+1. in the home directory, run <mkdir localFiles>, this is a directory the program expects to exist.
+2. without changing directory, run <mkdir localFiles/images>, this is another directory the program expects to exist.
+3. Run <ifconfig> and note the ip address for wlan0
+4. Run <sudo raspi-config> and using the arrow keys, navigate to "Interface Options", and press enter.
+5. Navigate to "I2 SSH" and press enter, enable SSH and press "Yes" when prompted. Wait until a notification saying SSH has been enabled appears.
+6. Navigate back to "Interface Options" and select "I2C" and similarly enable it like you did with SSH
+7. Navigate back to "Interface Options" and select "Legacy camera" and similarly enable it like you did with SSH
+8. Select "Finish" and exit the config tool.
+
+# Copying the application to the Pi
+These instructions are to be done on your main PC where a copy of sensorBox.py is downloaded. These instrucitons also assume that the program is in the downloads folder on the C: drive of a windows machine, and that windows powershell is being used.
+If this is not the case, <cd D:> or <cd <Drive Letter>> will switch to the proper drive, and any instance of <.\Downloads> can be changed to a path to the correct folder.
+
+1. run <cd .\Downloads>
+2. run <scp .\sensorBox.py pi@<Recorded IP Address>:./>
+
+# Setting up the program to start at runtime
+These instructions are to be done back on the pi connected to a keyboard or monitor. In this section, you will make a new systemd service to run sensorBox.py at start up.
+1. run <sudo nano /etc/systemd/system/sensor.service>
+2. In the editor, type the following text, note that it is case sensitive:
+
+[Unit]
+Description=sensor service
+After=network-online.target
+[Service]
+ExecStart=/usr/bin/python3 /home/pi/sensorBox.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+3. Enter ctrl+S to save, and ctrl+X to exit Nano.
+4. Restart the system's service manager with <sudo systemctl daemon-reload>
+5. Enable the new service with <sudo systemctl enable sensor.service>
+6. Turn off the pi with <sudo halt>, and disconnect it from the keyboard and monitor, next time it is powered on, the script should start at launch.
+
+Note- Copying over files can be done in a single SCP command, no need to ssh into the pi, then scp out of the pi. Just give those instructions in one line here.
+
+# Retrieving images
+These steps are to be done on a regular desktop
+
+Running the command <scp -r pi@<IP address>:./localFiles/images> on a powershell or bash terminal will retrieve the images from the pi.
+Running <ssh -e "rm ./localFiles/images"> to clear the cache of images, and prevent storage from filling up
--- sensorBox.py
+++ sensorBox.py
@@ -1,16 +1,14 @@
+from lib2to3.pgen2 import token
 import os
-from os import listdir
-from os.path import isfile
+import re
 import subprocess
 from datetime import time
 from datetime import datetime
 import time
 from scd30_i2c import SCD30
 import requests
-from google.oauth2 import service_account
-
-#Testing Import
-import random
+from oauth2client.service_account import ServiceAccountCredentials
+from picamera import PiCamera
 
 class SensorBox():
     def __init__(self):
@@ -18,7 +16,7 @@ class SensorBox():
         self.INTERVAL = 1800 #nterval to wait between sensor reads, in seconds
         self.sensor = SCD30()
 
-        self.URL = "https://firestore.googleapis.com/v1/projects/biosensorbox-c9334/databases/(default)/documents/<Date>"
+        self.URL = "https://firestore.googleapis.com/v1/projects/biosensorbox-c9334/databases/(default)/documents/<Date>/"
         self.serviceEndpoint = "https://firestore.googleapis.com"
         self.authEndpoint = "https://www.googleapis.com/auth/datastore"
         #Authentication for HTTP requests
@@ -26,11 +24,12 @@ class SensorBox():
         serviceAccountFile = 'home/pi/biosensorbox-c9334-478a6728ce7e.json'
         #serviceAccountFile = './biosensorbox-c9334-478a6728ce7e.json'
         #This credentials object is the oauth2 that I need to get/extract a token from it and pass that along to the HTTP
-        self.credentials = service_account.Credentials.from_service_account_file( serviceAccountFile, scopes=scope)
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name( serviceAccountFile, scope)
 
         #Globals
         self.localData = 0 #If 1, then there is sensor data saved to the disk
         self.logFile = 'home/pi/localFiles/log.txt'
+        self.camera = PiCamera()
         #self.logFile = './localFiles/log.txt'
 
     def readSensor(self):
@@ -80,7 +79,7 @@ class SensorBox():
                 reading[1] = float(reading[1])
                 reading[2] = float(reading[2])
                 #Post the stored file into the database, the last entry is the time when the data was originally recorded
-                self.sendToDatabase(reading, reading[-1])
+                self.sendToDatabase(reading, True)
                 #sleep for three seconds to keep the database from being overwhelmed
                 time.sleep(3)
             #Clear the file once all data is extracted by setting the cursor to position 0 and truncating
@@ -88,42 +87,112 @@ class SensorBox():
         localFile.close()
 
 
-    def sendToDatabase(self, reading, time = str(datetime.now()) ):
-        #Assemble API URL and a data object
-        documentURL = self.URL + '?documentId=' + time
-        data = {
-            "fields": {
-                "co2": {"integerValue":int(reading[0]) },
-                "temp": {"integerValue":int(reading[1]) },
-                "humidity": {"integerValue":int(reading[2]) },
-                "time": {"stringValue":time }
-            }
-        }
-        #Headers using service account credentials
-        headers = {"Bearer": self.credentials.token}
+    def sendToDatabase(self, reading, storedTime=False):
+        #If storedTime is true, then the last(4th) entry will have a time stored as a string
+        if storedTime:
+            CurrentTime = reading[-1]
+        else:
+            CurrentTime = str(datetime.now())
+        print(CurrentTime)
+        #Strip the current time to a useful random number for an ID
+        mask = re.sub(r"\s+", "", CurrentTime)
+        mask = re.sub(r":+", "", mask)
+        mask = re.sub(r"\-+", "", mask)
+        mask = re.sub(r"\.+", "", mask)
+        #Do to a quirk of update masks, the mask needs to have an alphabetic character in there, a purely numeric mask fails
+        mask = "A" + mask
+        #Assemble the URL and update mask
+        updateMask = "?updateMask.fieldPaths=" + mask
+        CO2Path = self.URL + "CO2" + updateMask
+        tempPath = self.URL + "Temperature" + updateMask
+        humPath = self.URL + "Humidity" + updateMask
 
-        #createResp = requests.post(self.URL, json=data, headers=headers)
-        #createResp = requests.post(self.URL, json=data)
-        createResp = requests.post(documentURL, json=data)
-        print(createResp.status_code)
+        #Headers using service account credentials, first refreshes the token
+        self.credentials.get_access_token()
+        #print(self.credentials.access_token)
+        #headers = "Bearer " + self.credentials.access_token
+
+        #Assemble data objects for each reading and append them to the relevant docs.
+        COData = {
+            "fields":{
+                mask:{
+                    "mapValue":{
+                        "fields":{
+                            "CO2": {"doubleValue":reading[0]},   
+                            "Time": {"stringValue": CurrentTime}
+                        }
+                    }
+                }
+            }
+
+        }
+        resp = requests.patch(CO2Path,json=COData) #, headers={"Authorization": headers})
+        print(resp.status_code)
         #Output response as a logFile entry
         logFile = open(self.logFile, 'a')
-        logFile.write(str(createResp.status_code) + " " + time + "/n")
+        logFile.write(str(resp.status_code) + " " + CurrentTime + "\n")
         logFile.close()
+        time.sleep(3)
+
+        tempData = {
+            "fields":{
+                mask:{
+                    "mapValue":{
+                        "fields":{
+                            "Temperature": {"doubleValue":reading[1]},   
+                            "Time": {"stringValue": CurrentTime}
+                        }
+                    }
+                }
+            }
+        }
+        resp = requests.patch(tempPath,json=tempData) #, headers={"Authorization": headers})
+        print(resp.status_code)
+        #Output response as a logFile entry
+        logFile = open(self.logFile, 'a')
+        logFile.write(str(resp.status_code) + " " + CurrentTime + "\n")
+        logFile.close()
+        time.sleep(3)
+
+        humData = {
+            "fields":{
+                mask:{
+                    "mapValue":{
+                        "fields":{
+                            "Humidity": {"doubleValue":reading[2]},   
+                            "Time": {"stringValue": CurrentTime}
+                        }
+                    }
+                }
+            }
+        }
+        resp = requests.patch(humPath,json=humData) #, headers={"Authorization": headers})
+        print(resp.status_code)
+
+        #Output response as a logFile entry
+        logFile = open(self.logFile, 'a')
+        logFile.write(str(resp.status_code) + " " + CurrentTime + "\n")
+        logFile.close()
+    
+    #Using a raspberry pi spycam, take a photo and store it to home/pi/localFiles/photos/<CurrentTime>.jpg
+    def takePhoto(self):
+        localFile = 'home/pi/localFiles/images' + str(datetime.now()) + '.jpg'
+        #localFile = './localFiles/images' + str(datetime.now()) + '.jpg'
+        self.camera.resolution = (1024, 768)
+        self.camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        self.camera.capture(localFile)
 
     def run(self):
         #Loop set-up
         self.sensor.set_measurement_interval(self.INTERVAL)
         self.sensor.start_periodic_measurement()
 
-        #Pause until close to XX:00 or XX:30, then enter the loop
-        #Expression should give a positive number between 1 and 30
-        #sleepTime = 30 - (datetime.now().time().minute % 30 )
-        #sleepTime *= 60 #Convert to seconds
-        #time.sleep( sleepTime )
         while(True):
             if( self.sensor.get_data_ready() ):
                 self.processData( self.readSensor() )
+                self.takePhoto()
             #Sleep at the end of the loop, as a stopgap
             time.sleep(self.INTERVAL)
 
